9c3890afdb31db2a3f6ea005eee516c2fb7572df
Shawon Gazi <gazishawon999@gmail.com>
2026-01-13 22:36:25 +0600

    Create variable_and_constant_declaration.c

---
+++ variable_and_constant_declaration.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main ()
+{
+  int playerscore=95;
+  printf("%d\n",playerscore);
+  
+  int marks=100;
+  printf("%d\n",marks);
+  
+  const double pi=3.1416;
+  printf("%lf\n", pi);
+  
+  char name[]="SHAWON GAZI";
+  printf("%s",name );
+  
+  return 0;
+}
